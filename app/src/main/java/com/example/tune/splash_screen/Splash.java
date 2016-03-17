a536bbfe0cc05110b10f0ae016dbf4cbd13a2ce4
meshnesh <tonnie.nesh@gmail.com>
2016-03-17 03:01:15 +0300

    Started the application and splash activity

---
+++ app/src/main/java/com/example/tune/splash_screen/Splash.java
@@ -0,0 +1,17 @@
+package com.example.tune.splash_screen;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by tune on 17/03/16.
+ */
+public class Splash extends Activity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+    }
+}
